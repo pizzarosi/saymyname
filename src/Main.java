12e9b75a3685731295b20ccaf3dfe99a7784ea39
Nicholas <clerklypizza315@gmail.com>
2023-08-02 19:46:20 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean productAvailable=true;
+        if(productAvailable=true){
+            System.out.println("You can buy this");
+        } else {
+            System.out.println("You can not buy this");
+        }
+
+    }
+
+}
